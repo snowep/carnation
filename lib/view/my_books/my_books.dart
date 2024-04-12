8de200abca0cf09161379547d2fe72db03a53da6
Ferdinand Wullur <snowep66@live.com>
2024-04-12 17:01:42 +0800

    ui: Update AppBar title & feat: Implement book list display from database
    - Updated the app bar title to "Bookshelf" for better user representation.
    - Implemented book list display, fetching books from the database and displaying them in a list style.
      * This allows users to see all their books at once.

--- lib/view/my_books/my_books.dart
+++ lib/view/my_books/my_books.dart
@@ -1,4 +1,6 @@
 import 'package:carnation/view/my_books/add_book.dart';
+import 'package:carnation/view/my_books/details/details_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MyBooksScreen extends StatefulWidget {
@@ -13,12 +15,39 @@ class _MyBooksScreenState extends State<MyBooksScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Books'),
+        title: const Text('Bookshelf'),
       ),
-      body: const Center(
-        child: Text(
-          'My Books Page',
-          style: TextStyle(fontSize: 24),
+      body: Center(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('books').snapshots(),
+          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => BookDetailsScreen(book: data),
+                      ),
+                    );
+                  },
+                  child: ListTile(
+                    title: Text(data['title']),
+                    subtitle: Text('ISBN: ${data['isbn']}'),
+                  ),
+                );
+              }).toList(),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
